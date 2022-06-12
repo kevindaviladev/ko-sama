cd492f0b33dbfdaee02dbbea1b01d68a05a9bfd4
Kevin Davila <kfariidavila@gmail.com>
2022-06-11 23:28:41 -0500

    added interceptor to first load of services

--- src/app/interceptors/directus.interceptor.ts
+++ src/app/interceptors/directus.interceptor.ts
@@ -5,32 +5,42 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { concatMap, delay, retryWhen } from 'rxjs/operators';
+import { Observable, of, throwError, timer } from 'rxjs';
+import { concatMap, delay, mergeMap, retryWhen, tap } from 'rxjs/operators';
 
 export const retryCount = 3;
 export const retryWaitMilliSeconds = 1000;
 
 @Injectable()
 export class DirectusInterceptor implements HttpInterceptor {
+  retryDelay = 2000;
+  retryMaxAttempts = 2;
+
   constructor() {}
 
   intercept(
-    request: HttpRequest<unknown>,
+    request: HttpRequest<any>,
     next: HttpHandler
-  ): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(
-      retryWhen((error) =>
-        error.pipe(
-          concatMap((error, count) => {
-            if (count <= retryCount && error.status == 503) {
-              return of(error);
-            }
-            return throwError(error);
-          }),
-          delay(retryWaitMilliSeconds)
-        )
-      )
-    );
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(this.retryAfterDelay());
+  }
+
+  retryAfterDelay(): any {
+    return retryWhen((errors) => {
+      return errors.pipe(
+        mergeMap((err, count) => {
+          // throw error when we've retried ${retryMaxAttempts} number of times and still get an error
+          if (count === this.retryMaxAttempts) {
+            return throwError(err);
+          }
+          return of(err).pipe(
+            tap((error) =>
+              console.log(`Retrying ${error.url}. Retry count ${count + 1}`)
+            ),
+            mergeMap(() => timer(this.retryDelay))
+          );
+        })
+      );
+    });
   }
 }
